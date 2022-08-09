051ba29c9cf2b97a375deb5b22a6cddc03d7e189
Tomislav Mikšik <64797760+tomislavmiksik@users.noreply.github.com>
2022-08-09 21:48:30 +0200

    simple navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent, },
+  { path: 'articles', component: ArticlesPageComponent, },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
